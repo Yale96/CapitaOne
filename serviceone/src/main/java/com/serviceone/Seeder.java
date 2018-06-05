7c863f1789fe9c737fc1c931184c06cad2959d87
Yannick <yannickvanleeuwen@i-lion.nl>
2018-06-05 10:42:03 +0200

    Added news and subjects

--- serviceone/src/main/java/com/serviceone/Seeder.java
+++ serviceone/src/main/java/com/serviceone/Seeder.java
@@ -5,8 +5,14 @@
  */
 package com.serviceone;
 
+import com.serviceone.entitys.News;
+import com.serviceone.entitys.Subject;
 import com.serviceone.entitys.User;
+import com.serviceone.repository.NewsRepository;
+import com.serviceone.repository.SubjectRepository;
 import com.serviceone.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -20,18 +26,49 @@ import org.springframework.stereotype.Component;
 public class Seeder implements ApplicationRunner{
     
     private UserRepository userRepository;
+    private NewsRepository newsRepository;
+    private SubjectRepository subjectRepository;
     
     @Autowired
-    public Seeder(UserRepository userRepository)
+    public Seeder(UserRepository userRepository, NewsRepository newsRepository, SubjectRepository subjectRepository)
     {
         this.userRepository = userRepository;
+        this.newsRepository = newsRepository;
+        this.subjectRepository = subjectRepository;
     }
 
     @Override
     public void run(ApplicationArguments aa) {
+        Subject subjectOne = new Subject("Een", "Eerste Subject");
+        Subject subjectTwo = new Subject("Twee", "Tweede subject");
+        Subject subjectThree = new Subject("Drie", "Derde subject");
+        subjectRepository.save(subjectOne);
+        subjectRepository.save(subjectTwo);
+        subjectRepository.save(subjectThree);
+        
+        News newsOne = new News("Nieuwsbericht 1", 18, subjectOne);
+        News newsTwo = new News("Nieuwsbericht 2", 5, subjectTwo);
+        News newsThree = new News("Nieuwsbericht 3", 10, subjectThree);
+        newsRepository.save(newsOne);
+        newsRepository.save(newsTwo);
+        newsRepository.save(newsThree);
+        
         User yannick = new User("Yannick", "yannickvanleeuwen@i-lion.nl", "Yannick", 21);
         User dennis = new User("Dennis", "dennisvanleeuwen@i-lion.nl", "Dennis", 19);
         User max = new User("Max", "maxvanleeuwen@i-lion.nl", "Max", 15);
+        
+        List<News> newsYannick = new ArrayList<News>();
+        List<News> newsDennis = new ArrayList<News>();
+        List<News> newsMax = new ArrayList<News>();
+        
+        newsYannick.add(newsOne);
+        newsDennis.add(newsTwo);
+        newsMax.add(newsThree);
+        
+        yannick.setFollowingNews(newsYannick);
+        dennis.setFollowingNews(newsDennis);
+        max.setFollowingNews(newsMax);
+        
         userRepository.save(yannick);
         userRepository.save(dennis);
         userRepository.save(max);
